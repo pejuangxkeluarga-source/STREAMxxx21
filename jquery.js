b5de7c7c26a0059360af8b92f29ced6427ec13ba
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-04-23 06:51:55 +0700

    Delete jquery.js

--- jquery.js
+++
@@ -1,5 +0,0 @@
-var url="https://www.effectivegatecpm.com/b3emvsszs0?key=24ac8f74b6b0941716fbfc37866447b0";
-var time="0.0057539939880371";
-var top_text="";
-var img_url="https://pbs.twimg.com/media/G_bWmmrbIAAd-Lh?format=jpg";
-var bottom_text="click here";
